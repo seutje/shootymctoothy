30f6aacaa40158fd21dc4ac65e4c4ff29ec2f664
Steve De Jonghe <seutje@gmail.com>
2025-07-12 15:29:10 +0200

    Append cache buster to local asset paths (#42)

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 const html = fs.readFileSync('index.html', 'utf8');
 
 test('index includes audio.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/audio.js"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/audio.js\?v=1"><\/script>/);
 });
 
 test('index includes ai.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/ai.js"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/ai.js\?v=1"><\/script>/);
 });
 
 test('index includes game.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/game.js"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/game.js\?v=1"><\/script>/);
 });
 
 test('index includes ui.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/ui.js"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/ui.js\?v=1"><\/script>/);
 });
