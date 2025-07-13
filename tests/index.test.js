f99669f8a6d2e6c2be6ac5492b5959401618996c
seutje <seutje@gmail.com>
2025-07-13 11:28:34 +0200

    Version bump.

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 const html = fs.readFileSync('index.html', 'utf8');
 
 test('index includes audio.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/audio.js\?v=1"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/audio.js\?v=2"><\/script>/);
 });
 
 test('index includes ai.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/ai.js\?v=1"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/ai.js\?v=2"><\/script>/);
 });
 
 test('index includes game.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/game.js\?v=1"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/game.js\?v=2"><\/script>/);
 });
 
 test('index includes ui.js script', () => {
-  expect(html).toMatch(/<script src="assets\/js\/ui.js\?v=1"><\/script>/);
+  expect(html).toMatch(/<script src="assets\/js\/ui.js\?v=2"><\/script>/);
 });
