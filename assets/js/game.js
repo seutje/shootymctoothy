1c77ae03a26005889315a7ac13748939243ded76
Steve De Jonghe <seutje@gmail.com>
2025-07-13 12:37:32 +0200

    Rotate sun 180° to flip shadows

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,8 +33,8 @@ scene.add(ambientLight);
 
 // Create a dimmer point light that represents the sun.
 const sunLight = new THREE.PointLight(0xffffff, 0.3, 0);
-// Rotate the sun light ninety degrees counter-clockwise around the scene so it matches the sky box sun.
-sunLight.position.set(-50, 100, 50);
+// Rotate the sun light one hundred eighty degrees around the scene so shadows are cast to the opposite side.
+sunLight.position.set(-50, 100, -50);
 // Allow the sun light to cast shadows.
 sunLight.castShadow = true;
 // Add the sun light to the scene.
